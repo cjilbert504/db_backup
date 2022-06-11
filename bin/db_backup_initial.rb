a3fcf6e3a3571e5dffca92e675511b7f6db4edb4
Collin Jilbert <cjilbert504@gmail.com>
2022-06-11 11:33:05 -0500

    bring in optparse; set up initial cli options

--- bin/db_backup_initial.rb
+++ bin/db_backup_initial.rb
@@ -1,16 +1,40 @@
 #!/usr/bin/env ruby
 
-database = ARGV.shift
-username = ARGV.shift
-password = ARGV.shift
-end_of_iter = ARGV.shift
+# Bring OptionParser in
+require "optparse"
 
-if end_of_iter.nil?
-  backup_file = database + Time.now.strftime "%Y%m%d"
-else
-  backup_file = database + end_of_iter
+options = {}
+option_parser = OptionParser.new do |opts|
+
+  # Create a switch
+  opts.on "-i", "--iteration" do
+    options[:iteration] = true
+  end
+
+  # Create a flag
+  opts.on "-u USER" do |user|
+    options[:user] = user
+  end
+
+  opts.on "-p PASSWORD" do |password|
+    options[:password] = password
+  end
 end
 
-`mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
-`gzip #{backup_file}.sql`
-end
+option_parser.parse!
+puts options.inspect
+
+# database = ARGV.shift
+# username = ARGV.shift
+# password = ARGV.shift
+# end_of_iter = ARGV.shift
+
+# if end_of_iter.nil?
+#   backup_file = database + Time.now.strftime "%Y%m%d"
+# else
+#   backup_file = database + end_of_iter
+# end
+
+# `mysqldump -u#{username} -p#{password} #{database} > #{backup_file}.sql`
+# `gzip #{backup_file}.sql`
+# end
