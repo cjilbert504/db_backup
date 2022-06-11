bafedbd84096f4e23f14d8a5134603b5f47bcd5b
Collin Jilbert <cjilbert504@gmail.com>
2022-06-11 14:20:55 -0500

    revert

--- bin/db_backup_initial.rb
+++ bin/db_backup_initial.rb
@@ -1,21 +1,10 @@
 #!/usr/bin/env ruby
 
-class Dog
-  attr_reader :name
-  def initialize(name)
-    @name = name
-  end
-end
-
 # Bring OptionParser in
 require "optparse"
 
 options = {}
 option_parser = OptionParser.new do |opts|
-
-  opts.accept(Dog) do |name|
-    Dog.new(name)
-  end
 
   # Create a switch
   opts.on "-i", "--iteration" do
@@ -27,11 +16,7 @@ option_parser = OptionParser.new do |opts|
     options[:user] = user
   end
 
-  # Example command-line usage:
-  # ruby db_backup/bin/db_backup_initial.rb -u Bob.bob -p McGruff
-  # puts options.inspect
-  # => {:user=>"Bob.bob", :password=>#<Dog:0x00007ff389823f78 @name="McGruff">}
-  opts.on "-p PASSWORD", Dog do |password|
+  opts.on "-p PASSWORD" do |password|
     options[:password] = password
   end
 end
